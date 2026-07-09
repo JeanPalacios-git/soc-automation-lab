33be52a4093104a6cdca1f753e6baaaf1b9fba14
Jean Palacios <297518101+JeanPalacios-git@users.noreply.github.com>
2026-07-09 14:41:26 -0600

    Implemented Wazuh Api authentication

---
+++ src/soc_tool/api/auth.py
@@ -0,0 +1,39 @@
+"""
+Authentication utilities for the Wazuh API.
+"""
+
+import requests
+import urllib3
+
+from requests.auth import HTTPBasicAuth
+
+from soc_tool.config.settings import settings
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def authenticate() -> str:
+    """
+    Authenticate to the Wazuh API and return a JWT token.
+    """
+
+    url = f"{settings.host}:{settings.port}/security/user/authenticate"
+
+    response = requests.get(
+        url=url,
+        auth=HTTPBasicAuth(
+            settings.username,
+            settings.password,
+        ),
+        verify=settings.verify_ssl,
+        timeout=settings.timeout,
+    )
+
+    print(response.status_code)
+    print(response.text)
+
+    response.raise_for_status()
+
+    token = response.json()["data"]["token"]
+
+    return token
---
+++ test_auth.py
@@ -0,0 +1,5 @@
+from src.soc_tool.api.auth import authenticate
+
+token = authenticate()
+
+print(token)
